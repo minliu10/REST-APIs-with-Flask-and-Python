11f4b9204a6270ca304726eaacb718a02089e68e
minliu10 <serendipitous251@gmail.com>
2020-12-21 23:47:49 +0800

    Creating a new model: StoreModel- 102

--- section6/code/resources/item.py
+++ section6/code/resources/item.py
@@ -4,13 +4,18 @@ from models.item import ItemModel
 
 
 class Item(Resource):
-    TABLE_NAME = 'items'
-
     parser = reqparse.RequestParser()
     parser.add_argument('price',
         type=float,
         required=True,
         help="This field cannot be left blank!"
+    )
+
+    parser = reqparse.RequestParser()
+    parser.add_argument('store_id',
+        type=int,
+        required=True,
+        help="Every item needs a store id."
     )
 
     @jwt_required()
@@ -26,7 +31,7 @@ class Item(Resource):
 
         data = Item.parser.parse_args()
 
-        item = ItemModel(name, data['price'])
+        item = ItemModel(name, data['price'], data['store_id'])
 
         try:
             item.save_to_db()
@@ -49,7 +54,7 @@ class Item(Resource):
         
         item = ItemModel.find_by_name(name)
         if item is None:
-            item = ItemModel(name, data['price'])
+            item = ItemModel(name, **data)   # **data = data['price'], data['store_id']
         else:
             item.price = data['price']
         item.save_to_db()

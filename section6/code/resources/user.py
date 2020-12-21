2878766c540a0a508b87271e7db1d12356b2a2c7
minliu10 <serendipitous251@gmail.com>
2020-12-21 23:09:17 +0800

    Implementing the UserModel using SQLAlchemy- 99

--- section6/code/resources/user.py
+++ section6/code/resources/user.py
@@ -3,7 +3,6 @@ from flask_restful import Resource, reqparse
 from models.user import UserModel
 
 class UserRegister(Resource):
-    TABLE_NAME = 'users'
 
     parser = reqparse.RequestParser()
     parser.add_argument('username',
@@ -23,13 +22,7 @@ class UserRegister(Resource):
         if UserModel.find_by_username(data['username']):
             return {"message": "User with that username already exists."}, 400
 
-        connection = sqlite3.connect('data.db')
-        cursor = connection.cursor()
-
-        query = "INSERT INTO {table} VALUES (NULL, ?, ?)".format(table=self.TABLE_NAME)
-        cursor.execute(query, (data['username'], data['password']))
-
-        connection.commit()
-        connection.close()
+        user = UserModel(**data)
+        user.save_to_db()
 
         return {"message": "User created successfully."}, 201

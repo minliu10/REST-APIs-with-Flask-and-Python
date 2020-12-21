00eab3eac6346850bf3149b903bf9ff914132a20
minliu10 <serendipitous251@gmail.com>
2020-12-21 23:24:33 +0800

    Easily displaying the ItemList resource with SQLAlchemy- 100

--- section6/code/models/item.py
+++ section6/code/models/item.py
@@ -1,9 +1,8 @@
-import sqlite3
 from db import db
 
 class ItemModel(db.Model):
     __tablename__ = 'items'
-    
+
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(80))
     price = db.Column(db.Float(precision=2))
